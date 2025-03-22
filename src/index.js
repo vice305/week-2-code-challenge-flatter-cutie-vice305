52d138ce6ae3e9653e7c4cdb22c9ab91fc3f6dc4
christopher <vice7440@gmail.com>
2025-03-22 13:54:40 +0300

    selecting elements and fetch

--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
 // Your code here
+const characterBar = document.getElementById("character-bar");
+const detailedInfo = document.getElementById("detailed-info");
+const characterName = document.getElementById("name");
+const characterImage = document.getElementById("image");
+const characterVotes = document.getElementById("vote-count");
+const voteForm = document.getElementById("votes-form");
+const resetButton = document.getElementById("reset-btn");
+const characterForm = document.getElementById("character-form")
+
+function fetchCharacters() {
+    fetch('http://localhost:3000/characters')
+        .then(response => response.json())
+        .then(data => {
+            characters = data;
+            renderCharacterBar();
+        })
+        .catch(error => console.error('Error fetching characters:', error));
+}
